0c6b0138d20e577cb0e0042e636d15a35a868324
svnoliveira <srkuran@gmail.com>
2023-07-06 19:55:07 -0300

    fix: corrigido erro de tipagem do estado selected movie

--- src/providers/MovieContext/@types.tsx
+++ src/providers/MovieContext/@types.tsx
@@ -1,3 +1,5 @@
+import { IUserReview } from "../UserContext/@types"
+
 export type TMovieScore = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 export type TmovieGenre = "ficção" | "comédia" | "drama"
 
@@ -21,7 +23,7 @@ export interface ISelectedMovie {
     duration: number
     synopsis: string
     image: string
-    reviews: IReview[]
+    reviews: IUserReview[]
 }
 
 export interface IMovieWithReviews {
