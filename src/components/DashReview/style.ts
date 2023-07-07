163030265516835d5ac45f4579a0f9ff9454c08b
svnoliveira <srkuran@gmail.com>
2023-07-07 03:51:24 -0300

    style: estilização do comentário do usuário logado

--- src/components/DashReview/style.ts
+++ src/components/DashReview/style.ts
@@ -14,11 +14,57 @@ export const StyledDashReview = styled.section`
         margin: 2rem 0 3rem 0;
     }
 
+    .reviewContainer{
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column-reverse;
+        align-items: center;
+        flex-wrap: nowrap;
+        padding: clamp(1rem, 0.6133rem + 2.2099vw, 3rem);
+        min-height: 12rem;
+        row-gap: 3rem;
+    }
+
+    .reviewButtonsContainer{
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        gap: 1rem;
+    }
+
+    .reviewButtonsContainer > div{
+        display: flex;
+        gap: 0.5rem;
+        margin-right: 2rem;
+    }
+
+    .reviewButtonsContainer > button{
+        background: transparent;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        transition: 0.3s;
+    }
+
+    .reviewButtonsContainer > button :hover{
+        transform: translate(3px, 2px) rotate(1deg);
+    }
+
     @media (min-width:770px) {
         padding: 0 12% 0 12%;
+        
         h1{
             font-size: 2.75rem;
             font-family: var(--FPoppins);
+        }
+
+        .reviewContainer > p {
+            width: 50%;
+        }
+        
+        .reviewContainer{
+            flex-direction: row;
+            flex-wrap: nowrap;
         }
     }
 `
