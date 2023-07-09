8d022bc21ed93e983403477fde9794ba1907a46b
svnoliveira <srkuran@gmail.com>
2023-07-09 12:46:13 -0300

    fix(style): adicionado overflow auto nos cards

--- src/components/DashReviewCard/style.ts
+++ src/components/DashReviewCard/style.ts
@@ -10,7 +10,7 @@ export const StyledDashReviewCard= styled.li`
      padding: 1rem  0.5rem;
      margin: 0.5rem 0 1rem 0;
      text-align: center;
-
+     overflow-y: auto;
      > :first-child {
          display: flex;
      }
@@ -34,8 +34,13 @@ export const StyledDashReviewCard= styled.li`
         display: flex;
          flex-direction: column;
          align-items: center;
-         height: fit-content;;
+         height: fit-content;
+        
          width: 100%;
+      }
+      .divDescription > p{
+         overflow: auto;
+         margin-bottom: 2rem;
       }
 
 @media (min-width:770px) {
