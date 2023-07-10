24d06d5240e1208b3f2cd65100bf64df2ee86f5c
svnoliveira <srkuran@gmail.com>
2023-07-10 04:48:36 -0300

    big-fix: renderização de informações do banner home corrigida

--- src/fragments/HomeMovieName/index.tsx
+++ src/fragments/HomeMovieName/index.tsx
@@ -7,19 +7,21 @@ import {
 } from "../../styles/typography/typography"
 import { AiOutlineStar } from "react-icons/ai"
 import { StyledStarRating } from "../StarRating/style"
+import { UserContext } from "../../providers/UserContext"
 
 export const HomeMovieName = () => {
+  const { currentPath } = useContext(UserContext)
   const { movieList, selectedMovie, getAverageScoresByMovieId } =
     useContext(MovieContext)
     const score = getAverageScoresByMovieId(
-      (selectedMovie ? selectedMovie.id : movieList[0].id)
+      (currentPath !== "/" ? selectedMovie!.id : movieList[0].id)
     )
   return (
     <>
       {movieList && (
         <StyledHomeMovieName className="divName">
           <StyledTitleOne className="name">
-            {selectedMovie ? selectedMovie.name : movieList[0].name}
+            {currentPath !== "/" ? selectedMovie!.name : movieList[0].name}
           </StyledTitleOne>
           <StyledStarRating>
             <AiOutlineStar fill="#FFBB38" size="35px" />
--- src/fragments/HomeTag/index.tsx
+++ src/fragments/HomeTag/index.tsx
@@ -3,17 +3,19 @@ import { MovieContext } from "../../providers/MovieContext"
 import{StyledBtnGenre} from "../../styles/tags/tagGenre"
 import {StyledParagrOne } from "../../styles/typography/typography"
 import { StyledHomeMovieTag } from "./style"
+import { UserContext } from "../../providers/UserContext"
 
 export const HomeMovieTag = () => {
   
   const { movieList, selectedMovie } = useContext(MovieContext)
+  const { currentPath } = useContext(UserContext)
 
   return (
     <>
       {movieList && (
         <StyledHomeMovieTag>
-          <StyledBtnGenre>{selectedMovie? selectedMovie.type : movieList[0].type}</StyledBtnGenre>
-          <StyledParagrOne >{selectedMovie? selectedMovie.duration : movieList[0].duration}m</StyledParagrOne>
+          <StyledBtnGenre>{currentPath !== "/" ? selectedMovie!.type : movieList[0].type}</StyledBtnGenre>
+          <StyledParagrOne >{currentPath !== "/" ? selectedMovie!.duration : movieList[0].duration}m</StyledParagrOne>
         </StyledHomeMovieTag>
       )}
     </>
